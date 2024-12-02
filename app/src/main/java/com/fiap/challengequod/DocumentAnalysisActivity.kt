27a5e6e4c55f8bb37e07f457c8080609dfeab4c4
Rodrigomendesas <rodrigomendesas@gmail.com>
2024-12-02 15:40:36 -0300

    Adds pages inside each functionality; adds Success and Fail buttons; adds return button to return to the home view.

--- app/src/main/java/com/fiap/challengequod/DocumentAnalysisActivity.kt
+++ app/src/main/java/com/fiap/challengequod/DocumentAnalysisActivity.kt
@@ -2,6 +2,7 @@ package com.fiap.challengequod
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,6 +17,10 @@ class DocumentAnalysisActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnReturn = findViewById<Button>(R.id.btnReturn)
+        btnReturn.setOnClickListener {
+            finish()
         }
     }
 }
--- app/src/main/java/com/fiap/challengequod/RegisterUpdateActivity.kt
+++ app/src/main/java/com/fiap/challengequod/RegisterUpdateActivity.kt
@@ -1,6 +1,7 @@
 package com.fiap.challengequod
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,6 +14,10 @@ class RegisterUpdateActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnReturn = findViewById<Button>(R.id.btnReturn)
+        btnReturn.setOnClickListener {
+            finish()
         }
     }
 }
